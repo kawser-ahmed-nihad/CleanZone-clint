0a7845b0dd269d4e02a14ec3763768505941b104
kawser11223 <148429350+kawser11223@users.noreply.github.com>
2025-11-16 23:53:55 +0600

    Animated-slider added

--- src/Components/AnimatedSlider.jsx
+++ src/Components/AnimatedSlider.jsx
@@ -5,19 +5,19 @@ import "slick-carousel/slick/slick-theme.css";
 
 const slides = [
   {
-    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fwalk-pile-garbage&psig=AOvVaw0-SdBQPGFeqmW5MaRdnjnI&ust=1763400267467000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDztI2Y95ADFQAAAAAdAAAAABAE",
+    img: "https://plus.unsplash.com/premium_photo-1661964003668-f499e888f1b0?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Rising Urban Garbage Crisis",
     description: "Uncontrolled waste dumping is causing severe pollution, foul smells, and health risks in urban areas. Overflowing dustbins, plastic waste, clogged drains, and uncollected garbage are turning city spaces into high-risk zones for diseases and environmental damage.",
   },
   {
-    img: "https://i.postimg.cc/DyRFzSdV/Screenshot-2025-05-21-215937.png",
-    title: "Plant a Better Future",
-    description: "Take a step to save the Earth by planting trees.",
+    img: "https://images.unsplash.com/photo-1700732813265-e551398cb0bf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Community-Driven Clean-Up Initiatives",
+    description: "Local residents, youth, and volunteer groups are taking active roles in cleaning their neighborhoods. These campaigns promote awareness, responsibility, and teamwork—leading to visible improvement in community hygiene and reduction in random waste dumping.",
   },
   {
-    img: "https://i.postimg.cc/XJx4Nxxx/Screenshot-2025-05-21-220613.png",
-    title: "Nature in Your Home",
-    description: "Make your home calm, green, and refreshing.",
+    img: "https://images.unsplash.com/photo-1599278486038-419faa3a8c80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Steps Toward a Sustainable Future",
+    description: "Sustainability begins with smarter waste handling. Recycling plastic, composting organic waste, and using reusable products significantly reduce environmental damage. Smart bins, recycling programs, and renewable solutions create cleaner, healthier cities for future generations.",
   },
 ];
 
@@ -34,7 +34,7 @@ const sliderSettings = {
 
 const AnimatedSlider = () => {
   return (
-    <div className="mt-16">
+    <div className="">
       <Slider {...sliderSettings}>
         {slides.map((slide, index) => (
           <div key={index} className="relative h-[300px] md:h-[400px] w-full">
